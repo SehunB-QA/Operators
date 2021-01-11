84aab0c49fe8f81f667ef84549b205ef8a2d09c5
SehunB-QA <sbabatunde@qa.com>
2021-01-11 02:29:19 +0000

    Added my Java QA Operators solution program

---
+++ src/operators/Main.java
@@ -0,0 +1,17 @@
+package operators;
+
+
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		Operators calc = new Operators();
+		System.out.println("The added sum is : " + calc.Addtion(2, 4));
+		
+		System.out.println("The subtracted sum is : " + calc.Subtraction(4, 2));
+		
+		System.out.println("The divised sum is : " + calc.Division(10, 2));
+}
+
+}
---
+++ src/operators/Operators.java
@@ -0,0 +1,24 @@
+// Sehun Babatunde
+
+package operators;
+
+public class Operators {
+	
+	public int Addtion(int firstValue, int secondValue)
+	{
+		return firstValue + secondValue;
+	}
+	
+	public int Subtraction (int firstValue, int secondValue)
+	{
+		return firstValue - secondValue;
+	}
+	
+	public double Division (int firstValue, int secondValue)
+	{
+		double result;
+		return result = firstValue / secondValue;
+	}
+	
+
+}
